29e8540b25021304050a78a7b2f7f631488ca89f
poojangit <poojang147@gmail.com>
2025-07-18 13:00:17 +0530

    [POOJA] Start : Displayed Welcome message to Address book Program

---
+++ AddressBookMain.ts
@@ -0,0 +1,7 @@
+class AddressBookMain {
+    displayWelcomeMessage() : void{
+        console.log("🖐️  Welcome to my Address Book Program");
+    }
+}
+const addressBook = new AddressBookMain()
+addressBook.displayWelcomeMessage()
