1f72eabbf9099ed120aa7a6cc5eef12585e79690
poojangit <poojang147@gmail.com>
2025-07-28 12:25:21 +0530

    [POOJA] Testing : Added the testing methods for File Manager to test the files that is being added as json file

--- __tests__/AddressBook.test.ts
+++ __tests__/AddressBook.test.ts
@@ -38,4 +38,28 @@ describe("Address Class" , () => {
         expect(addressBook.getAllContacts().length).toBe(0)
     })
      
+    //Test for search contacts by city
+    test("Should return contacts by city", () => {
+        addressBook.addContact(contact)
+        const result = addressBook.searchByCity("Kodur")
+        expect(result.length).toBe(1)
+    })
+
+    //Test for sorting contacts by person name
+    test("should sort contacts by name", () => {
+        const contact2 = new Contact(
+            "Laksmi", 
+            "Muthyappa", 
+            "Street", 
+            "City", 
+            "ST", 
+            11111, 
+            1234567890, 
+            "laksmi@example.com"
+        );
+        addressBook.addContact(contact2);
+        addressBook.addContact(contact);
+        const sorted = addressBook.getSortedContactsByField("name");
+        expect(sorted[0].firstName).toBe("Deepika");
+    });
 })
